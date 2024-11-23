249533d87cf34853319b8405b271d9c4d279abbc
jhonVitor-rs <joaovitor.jvrs6@gmail.com>
2024-11-23 13:35:07 -0300

    feat: first commit

---
+++ database/migrations/1732307193242_create_addresses_table.ts
@@ -0,0 +1,25 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'addresses'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.uuid('id').primary()
+      table.string('public_place', 254).notNullable()
+      table.string('number', 6).notNullable()
+      table.string('neighborhood', 49).notNullable()
+      table.string('city', 49).notNullable()
+      table.string('state', 49).notNullable()
+      table.string('complement', 254).nullable()
+      table.string('zip_code', 14).notNullable()
+
+      table.timestamp('created_at').notNullable()
+      table.timestamp('updated_at').nullable()
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
